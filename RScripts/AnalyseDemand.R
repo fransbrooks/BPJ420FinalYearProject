f3bc7f79f89aea2f00881c2a7fc24d8aabb9e6b2
fransbrooks <frans.brooks1@gmail.com>
2014-10-01 18:52:29 +0200

    Final update of plots and scripts before hand-in of project

---
+++ RScripts/AnalyseDemand.R
@@ -0,0 +1,9 @@
+setwd("/home/frans/Documents/BPJ420/ProcessedDemand/shipments")
+w <- read.csv("shipments.csv")
+setwd("/home/frans/Documents/BPJ420/ProcessedDemand/Day11")
+x <- read.csv("Day11.csv")
+setwd("/home/frans/Documents/BPJ420/ProcessedDemand/Day3")
+y <- read.csv("Day3.csv")
+setwd("/home/frans/Documents/BPJ420/ProcessedDemand/Day5")
+z <- read.csv("Day5.csv")
+
